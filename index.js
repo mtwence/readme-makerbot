bd836b302c05f93aeae32177ece3a619ca3098ce
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-07-18 22:11:47 -0400

    add function to write to readme

--- index.js
+++ index.js
@@ -54,3 +54,9 @@ const questions = [
     },
 
 ];
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.error(err) : console.log("Hooray!")
+    })
+}
