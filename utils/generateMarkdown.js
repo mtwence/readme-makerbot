3ea0902735e125214db7ea780b23d53613bc137f
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-07-19 00:33:10 -0400

    fix reference error on line 19

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,23 +2,21 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(licenses) {
   switch(licenses) {
-    case 'MIT':
-      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
-    
-    case 'APACHE 2.0':
+    case "MIT":
+      return `[![License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    case "APACHE 2.0":
       return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
-    
-    case 'BSD 3':
+    case "BSD 3":
       return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
     default:
-      return '';
+      return "";
   }  
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(licenses) {
-  return `This repository is licensed by ${license} \n`
+  return `This repository is licensed by ${licenses} \n`
 }
 
 // TODO: Create a function to generate markdown for README
