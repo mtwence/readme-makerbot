5012488847e10cc5c4529ef43681b6bdb57f71d3
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-07-18 23:50:52 -0400

    start generateMarkdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -15,19 +15,24 @@ function renderLicenseBadge(licenses) {
   }  
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(licenses) {
+  return `This repository is licensed by ${license} \n`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.licenses)}
 
-`;
+## Description
+${data.description}
+
+## Table of Contents
+
+
 }
+
 
 module.exports = generateMarkdown;
