8f44979770e37d22b2dce4361ebc1da314bf69a7
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-07-18 22:23:43 -0400

    add MIT badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,15 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(licenses) {
+  switch(licenses) {
+    case 'MIT':
+        return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+    
+    case 'APACHE 2.0';
+      return 
 
+  }  
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
